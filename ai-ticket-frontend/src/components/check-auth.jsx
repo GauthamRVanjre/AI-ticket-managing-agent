88a6296aac21435093ab66c676e3485a200ed42e
theeliteclub2k <vanjregautham@gmail.com>
2025-06-22 12:56:23 +0530

    feat: check auth middeware

--- ai-ticket-frontend/src/components/check-auth.jsx
+++ ai-ticket-frontend/src/components/check-auth.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const CheckAuth = () => {
-  return <div>check-auth</div>;
+const CheckAuth = ({ children, protectedRoute }) => {
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (protectedRoute) {
+      if (!token) {
+        navigate("/login");
+      } else {
+        setLoading(false);
+      }
+    } else {
+      if (token) {
+        navigate("/");
+      } else {
+        setLoading(false);
+      }
+    }
+  }, [navigate, protectedRoute]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return children;
 };
 
 export default CheckAuth;
